54cb94bfd4e7e04c7eda0f8f9ef7c313812168e6
FoulTrip <davidvasquezmahecha@gmail.com>
2025-01-29 11:51:18 -0500

    fix: update main.ts to listen on configurable port and enable listening on all interfaces

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000)
-  // const port = process.env.PORT || 8080;
-  // await app.listen(port, '0.0.0.0');
+  // await app.listen(3000)
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
